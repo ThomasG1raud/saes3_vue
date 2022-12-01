0d8c1a8ce41f3e1841dbac750be8c327932be124
NexFord <nexford.bot@gmail.com>
2022-12-01 16:23:29 +0100

    modify the navbar

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,14 @@ export default {
 </script>
 
 <style>
+:root {
+  --light: #FFFFFF;
+  --dark: #333333;
+  --primary: #FF8080;
+  --secondary: #80FF80;
+  --tertiary: #8080FF
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -21,9 +29,9 @@ export default {
   text-align: center;
   color: #2c3e50;
 }
-
-nav {
-  padding: 30px;
+body {
+  margin: 0;
+  padding: 0;
 }
 
 nav a {
@@ -33,5 +41,13 @@ nav a {
 
 nav a.router-link-exact-active {
   color: #42b983;
+}
+
+button {
+  border: 2px solid black;
+  border-radius: 5px;
+  background-color: var(--light);
+  color: black;
+  padding: 5px;
 }
 </style>
