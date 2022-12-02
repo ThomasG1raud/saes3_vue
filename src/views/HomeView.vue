6de13fb639152df6abdaae71611a10e00a740e91
Julien Merveaux <jmerveau@iut-bm.univ-fcomte.fr>
2022-12-02 11:51:50 +0100

    add Image principal in Vitrine

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,17 @@
 <template>
   <div class="home">
+    <div class="imagePrincipal">
+      <img class="imgPrincipalVitrine" src="../assets/imagePrincipalVitrine.jpg">
+    </div>
     <MapView/>
+
     <div class="grid">
-      <CardActivityView image-card="logo.png" title-card="Activités" text-card="Découvrez les anciennes méthodes de la  sculpture, de la forge ou encore de la caLligraphie"/>
-      <CardActivityView image-card="logo.png" title-card="Spectacles" text-card="Assistez aux combats de preux chevaliers, ecoutez la musique des temps ancienS ou bien encore venez découvrir la vie des habitants du Moyen-age"/>
-      <CardActivityView image-card="logo.png" title-card="Boutiques, restaurations" text-card="prenez plaisir à découvrir la cuisine antique et ses saveurs authentique, ou offrez-vous un petit plaisir dans l'une de nos nombreuses boutiques"/>
+      <CardActivityView image-card="logo.png" title-card="Activités"
+                        text-card="Découvrez les anciennes méthodes de la  sculpture, de la forge ou encore de la caLligraphie"/>
+      <CardActivityView image-card="logo.png" title-card="Spectacles"
+                        text-card="Assistez aux combats de preux chevaliers, ecoutez la musique des temps ancienS ou bien encore venez découvrir la vie des habitants du Moyen-age"/>
+      <CardActivityView image-card="logo.png" title-card="Boutiques, restaurations"
+                        text-card="prenez plaisir à découvrir la cuisine antique et ses saveurs authentique, ou offrez-vous un petit plaisir dans l'une de nos nombreuses boutiques"/>
     </div>
     <hr style="margin-top: 300px">
     <img alt="Vue logo" src="../assets/logo.png">
@@ -29,8 +36,17 @@ export default {
 </script>
 
 <style>
-  .grid {
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
+
+  imagePrincipal {
+  width: 100%;
   }
+  .imgPrincipalVitrine {
+    width: 100%;
+    height: 50%;
+  }
+
+.grid {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+}
 </style>
