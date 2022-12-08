c23ba5d71f40c0c24e523632754ada2f66394928
Julien Merveaux <jmerveau@iut-bm.univ-fcomte.fr>
2022-12-08 15:40:31 +0100

    add profil prestataire

--- src/views/PrestataireProfilView.vue
+++ src/views/PrestataireProfilView.vue
@@ -1,57 +1,51 @@
 <template>
   <div id="card">
-      <div id="divImage-info">
-        <div id="divImage">
-          <img src="../assets/prestataire.png">
+    <div id="divImage-info">
+      <div id="divImage">
+        <img src="../assets/prestataire.png">
+      </div>
+      <div id="divInfo">
+        <div class="partie">
+          <p> Prenom :</p>
+          <p> Julien </p>
         </div>
-        <div id="divInfo">
-          <div class="section">
-            <div class="partie">
-              <div class="reponse">
-                <p> Prenom :</p>
-                <p> Julien</p>
-              </div>
-              <hr>
-            </div>
-            <div class="partie">
-              <div class="reponse">
-                <p> nom de votre stand :</p>
-                <p> Les mont d'Or de michel</p>
-              </div>
-              <hr>
-            </div>
-          </div>
-          <div class="section">
-            <div class ="partie">
-              <div class="reponse">
-                <p> Nom de famille :</p>
-                <p> Merveaux</p>
-              </div>
-              <hr>
 
-            </div>
-          </div>
-          <div class="section">
-            <div class="partie">
-              <div class="reponse">
-                <p> E-mail :</p>
-                <p> Julien.merveaux@hotmail.fr</p>
-              </div>
+        <hr>
 
+        <div class="partie">
+          <p> Nom de famille :</p>
+          <p> Merveaux </p>
+        </div>
 
-            </div>
-            <div class="partie">
-              <div class="reponse">
-                <p>Numéro de siren :</p>
-                <p> 2467354253</p>
-              </div>
+        <hr>
 
+        <div class="partie">
+          <p> E-mail:</p>
+          <p> Julien.Merveaux@hotmail.de </p>
+        </div>
 
-            </div>
-          </div>
+        <hr>
+
+        <div class="partie">
+          <p> Nom de votre stand : </p>
+          <p> La bonne saucisse de michelle </p>
+        </div>
+
+        <hr>
+
+        <div class="partie">
+          <p> siren</p>
+          <p> 17265301 </p>
         </div>
       </div>
-      <div id = "divService"></div>
+    </div>
+    <div class="rond" v-on:click="bool =!bool">
+      <img class="fleche" src="../assets/keyboard_double_arrow_up_black.svg">
+    </div>
+
+    <div id="divService" v-if="bool">
+      <p id="paragraphe">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+    </div>
   </div>
 </template>
 
@@ -65,14 +59,15 @@ export default {
       text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
       imagePrestataire: "prestataire.png",
       imageStand: "stand.png"
-    }
+    },
+    bool:false
   })
 }
 </script>
 
 <style scoped>
   #card {
-    height: 500px;
+
     margin: 100px;
   }
 
@@ -106,32 +101,19 @@ export default {
 
   #divService {
     width: 100%;
-    height: 36%;
     border-radius: 20px;
     border:solid;
     margin-top: 5%;
+    text-align: start;
+    padding: 20px;
+
   }
 
   img{
     width: 100%;
   }
 
-  .section {
-    display: flex;
-    width: 100%;
-    margin: 10px;
 
-  }
-
-  .partie {
-    display: flex;
-    justify-content: start;
-    align-items: center;
-    flex-direction: column;
-    height: 100%;
-    width: 50%;
-    margin-left: 10px;
-  }
 
   hr {
     width: 70%;
@@ -144,12 +126,24 @@ export default {
     margin-right: 20px;
   }
 
-  .reponse {
-    display: flex;
-    width: 100%;
+  .rond {
+    position: absolute;
+    translate: 900px -50px;
+    height: 100px;
+    width: 100px;
+    background-color: white;
+    border:solid;
+    border-radius: 50%;
   }
 
+  .fleche{
+    rotate: 180deg;
+  }
 
-
+  .partie {
+    display: flex;
+    width: 90%;
+    margin: 20px;
+  }
 
 </style>
